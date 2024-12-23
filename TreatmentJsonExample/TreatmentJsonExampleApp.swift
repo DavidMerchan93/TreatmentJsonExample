1ec1db98746d50d7944e0e12df58e41c23efe10e
David Merchan <davidmerchan10@gmail.com>
2024-12-23 14:29:52 -0500

    Initial Commit

---
+++ TreatmentJsonExample/TreatmentJsonExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreatmentJsonExampleApp.swift
+//  TreatmentJsonExample
+//
+//  Created by David Merchan on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TreatmentJsonExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
